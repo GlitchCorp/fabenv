d7be2d8d842d8adf8e5f68b08c64d8dd63ed5f62
Mariusz Masztalerczuk <mariusz@masztalerczuk.com>
2018-09-21 18:19:37 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fabenv',
+      version='0.1',
+      description='',
+      url='http://github.com/glichcorp/fabenv',
+      author='Mariusz Masztalerczuk',
+      author_email='mariusz@glichcorp.com',
+      license='MIT',
+      packages=['fabenv'],
+      zip_safe=False)
