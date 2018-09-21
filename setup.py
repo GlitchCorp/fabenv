0340057616ce06e8663d522989468722da46ded2
Mariusz Masztalerczuk <mariusz@masztalerczuk.com>
2018-09-21 18:20:44 +0200

    Typo

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='fabenv',
       version='0.1',
       description='',
-      url='http://github.com/glichcorp/fabenv',
+      url='http://github.com/glitchcorp/fabenv',
       author='Mariusz Masztalerczuk',
-      author_email='mariusz@glichcorp.com',
+      author_email='mariusz@glitchcorp.com',
       license='MIT',
       packages=['fabenv'],
       zip_safe=False)
